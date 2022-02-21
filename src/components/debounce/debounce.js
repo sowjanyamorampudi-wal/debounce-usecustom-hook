f683eb23ef58b1cd00f424c673d44f901c0d9d96
sowjanya <sowjanya.morampudi@westaglielabs.com>
2022-02-21 15:18:07 +0530

    tesing for one more input

--- src/components/debounce/debounce.js
+++ src/components/debounce/debounce.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 import useDebounce from "../../hooks/useDebounce";
 const Debounce = () => {
   const [value, setValue] = useState("");
-  const [secValue, setSecValue] = useState("");
   const [debounceValue, setDebounceValue] = useState("");
-  const [secDebounceValue, setSecDebounceValue] = useState("");
+  //   const [secValue, setSecValue] = useState("");
+  //   const [secDebounceValue, setSecDebounceValue] = useState("");
   const debounce = useDebounce();
 
   const changeHandler = (e) => {
@@ -12,20 +12,20 @@ const Debounce = () => {
     setValue(text);
     debounce(() => setDebounceValue(text), 1000);
   };
-  const changeHandlerSec = (e) => {
-    const secText = e.target.value;
-    setSecValue(secText);
-    debounce(() => setSecDebounceValue(secText), 5000);
-  };
+  //   const changeHandlerSec = (e) => {
+  //     const secText = e.target.value;
+  //     setSecValue(secText);
+  //     debounce(() => setSecDebounceValue(secText), 5000);
+  //   };
   return (
     <div>
       <input type="text" value={value} onChange={changeHandler} />
       <br />
-      <input type="text" value={secValue} onChange={changeHandlerSec} />
+      {/* <input type="text" value={secValue} onChange={changeHandlerSec} /> */}
       <h1>normal value: {value}</h1>
       <h1>debouncing value: {debounceValue}</h1>
-      <p>secValue:{secValue}</p>
-      <p>secDebounceValue:{secDebounceValue}</p>
+      {/* <p>secValue:{secValue}</p>
+      <p>secDebounceValue:{secDebounceValue}</p> */}
     </div>
   );
 };
